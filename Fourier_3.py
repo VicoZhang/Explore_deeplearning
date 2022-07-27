18daf43ad1dd2898f1ccecfd419d1832c4ceedb3
Vico Zhang <1248130720@qq.com>
2022-07-27 14:57:14 +0800

    fft的最终版本

--- Fourier_3.py
+++ Fourier_3.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy import fftpack
 from matplotlib import pyplot as plt
 
 # 产生两个工频周波
@@ -8,26 +7,20 @@ T_s = 1 / 1000  # 1ms
 T = 0.02  # 20ms
 w = 2 * np.pi / T
 t = np.arange(0, T, 0.001)  # 选择一个周波，在一个周波上采样 0.2/T_s 个点
-ft = np.sin(2 * w * t)
+ft = np.sin(3 * w * t)
 # plt.grid()
 # plt.plot(t, ft)
 # plt.show()
 
-fw = fftpack.fft(ft)
-fe = fftpack.fftfreq(int(0.02 / T_s), t[1] - t[0])
-plt.grid()
-# print(ft)
-# print(np.abs(fw))
-# print(fe)
-# plt.plot(fe[fe > 0], np.abs(fw[fe > 0]))
-plt.plot(fe, np.abs(fw))
-plt.show()
-# print("f_s = 1/T = {}Hz".format(fe[1]-fe[0]))
-# print("T_s = {}s".format(t[1]-t[0]))
+fw = np.abs(np.fft.fft(ft, norm="forward"))
+fe = np.fft.fftfreq(int(0.02 / T_s), t[1] - t[0])
 
-# 自行设计离散傅里叶变换：
-# fk = np.zeros_like(fw)
-# for k in range(20):
-#     for n in range(len(ft)):
-#         fk[k] += ft[n]*np.exp(-1j*2*np.pi*k*n/len(ft))
-# print(np.abs(fk))
+fw_positive = fw[fe >= 0]
+fw_positive[0] /= 2
+fe_positive = fe[fe >= 0]
+
+print(np.fft.fftshift(fw_positive))
+print(fe_positive)
+plt.grid()
+plt.plot(fe_positive, fw_positive)
+plt.show()
